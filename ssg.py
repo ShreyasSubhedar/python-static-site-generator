03bdda193b576a6f07175eca407ce7e60bb96708
Shreyas Subhedar <shreyas.subhedar@acquia.com>
2022-02-11 17:59:20 +0530

    DEVOPS-self | m1

---
+++ ssg.py
@@ -0,0 +1,14 @@
+import typer
+from ssg.site import Site
+
+
+def main(source="content", dest="dest"):
+    config = {
+        "source": source,
+        'dest': dest
+    }
+    site = Site(**config)
+    site.build()
+
+
+typer.run(main())
